dbffba4c7e7172136f0b74c5f2b48348a5f65640
Nir Tzezana <nitzezan@microsoft.com>
2021-10-12 15:31:12 +0300

    pass screen update

--- src/screens/PassScreen.jsx
+++ src/screens/PassScreen.jsx
@@ -26,9 +26,9 @@ const PassScreen = ({ playerLeftNumber, playerEliminatedNumber }) => {
     return (
         <StyledPassScreen className="animate__animated animate__zoomInLeft">
             <h1>You Passed!</h1>
-            <h2>{playerEliminatedNumber} users eliminated this round!</h2>
-            <h2>{playerLeftNumber} left!</h2>
-            <h2>Waiting for next round...</h2>
+            {/* <h2>{playerEliminatedNumber} users eliminated this round!</h2> */}
+            {/* <h2>{playerLeftNumber} left!</h2> */}
+            <h2>Waiting for all players to submit or fail to continue to the next round...</h2>
         </StyledPassScreen>
     )
 }
