bdd576a71235114d5bc78f14a6b6fdbbda42ea78
saalon <saharalon87@gmail.com>
2021-10-13 17:08:10 +0300

    playersStats

--- src/screens/GameOverScreen/GameOverScreen.jsx
+++ src/screens/GameOverScreen/GameOverScreen.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import { GameOverContainer } from './StyledGameOverScreen';
+import { PLAYER_WORTH } from '../../utils/constants';
 
-const GameOver = ({ winners }) => {
-    console.log({ winners });
+function numberWithCommas(num) {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+const playerWorthText = numberWithCommas(numberWithCommas(PLAYER_WORTH));
+
+const GameOver = ({ playersStats: { passedUsers, eliminatedUsers } }) => {
+    const passedText = `${(passedUsers > 1 ? `You and ${passedUsers} other competitors` : 'You')} have finished the game and won a shitty inflated Korean money`;
+    const eliminatedText = `${eliminatedUsers} players were eliminated, each one of them worths ${playerWorthText}₩ (Korean Won)`;
+    const convertText = `Wanna know how much is ${numberWithCommas(eliminatedUsers * PLAYER_WORTH)}₩ ?`;
     return (
         <GameOverContainer>
             <h1>🏆 Congratulations 🏆</h1>
             <h2>You're this 💰💰💰 rich !</h2>
-            <h2>{ (winners?.length > 1 ? `You and ${winners.length} other competitors` : 'You') + ' have finished the game and won a shitty inflated Korean money' }</h2>
-            <h2>Wanna know how much is 45 billion "Korean Won"?</h2>
+            <h2>{ passedText }</h2>
+            <h2>{ eliminatedText }</h2>
+            <h2>{ convertText }</h2>
         </GameOverContainer>
     )
 }
