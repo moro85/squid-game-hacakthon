53ff90dbbd80bb876b7efab576a1e1caa0094d7e
Nir Tzezana <nitzezan@microsoft.com>
2021-10-13 11:13:22 +0300

    SSL FIX

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,6 @@
-const wsServer = window.location.host.replace("http://","ws://").replace("https://","wss://").replace("3000","8080");
-export const socket = new WebSocket(`ws://${wsServer}`, []);
-
+const wsServer = window.location.host.replace("3000","8080");
+const wsServerType = document.location.origin.indexOf("http://") > -1 ? "ws://" : "wss://";
+export const socket = new WebSocket(`${wsServerType}${wsServer}`, []);
 export const sendSocketMessage = (type, args) => {
     console.log({socket});
     console.log({type});
