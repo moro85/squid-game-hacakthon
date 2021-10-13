eec46987e70f69d6d153e4c7d15bd93530bcd6b9
saalon <saharalon87@gmail.com>
2021-10-13 20:29:27 +0300

    Update GameOverScreen.jsx

--- src/screens/GameOverScreen/GameOverScreen.jsx
+++ src/screens/GameOverScreen/GameOverScreen.jsx
@@ -11,14 +11,15 @@ const playerWorthText = numberWithCommas(numberWithCommas(PLAYER_WORTH));
 const GameOver = ({ playersStats: { passedUsers, eliminatedUsers } }) => {
     const passedText = `${(passedUsers > 1 ? `You and ${passedUsers} other competitors` : 'You')} have finished the game and won a shitty inflated Korean money`;
     const eliminatedText = `${eliminatedUsers} players were eliminated, each one of them worths ${playerWorthText}₩ (Korean Won)`;
-    const convertText = `Wanna know how much is ${numberWithCommas(eliminatedUsers * PLAYER_WORTH)}₩ ?`;
+    const moneySum = eliminatedUsers * PLAYER_WORTH;
+    const convertText = `Wanna know how much is ${numberWithCommas(moneySum)}₩ ?`;
     return (
         <GameOverContainer>
             <h1>🏆 Congratulations 🏆</h1>
             <h2>You're this 💰💰💰 rich !</h2>
             <h2>{ passedText }</h2>
             <h2>{ eliminatedText }</h2>
-            <h2>{ convertText }</h2>
+            <h2>{ convertText } <a href={`https://www.xe.com/currencyconverter/convert/?Amount=${moneySum}&From=KRW&To=USD`}>Click here</a></h2>
         </GameOverContainer>
     )
 }
